c8d2c3cb0aefa0c90946d3c83c434f583ac38395
Ezequielgit <Ezequiel927274@gmail.com>
2023-10-16 22:31:24 -0300

    1 Aula de JS” Git commit -m 1_Aula_de_JS”

---
+++ Aula1/Funçoes_e_variaveis.js
@@ -0,0 +1,39 @@
+var variavel1 = "Piranha"
+let variavel2 = 10
+const variavel3 = 50
+
+console.log("ola mundo")
+console.log(variavel1 + "kkkkkkk")
+console.log(variavel1, "\n vc está presa por",variavel2,"anos por excesso de gostosura 🥰")
+console.log()//Para dar espaço (O \n não funcionou)
+
+console.log(variavel2 + variavel3)
+console.log(variavel2 / variavel3)
+console.log(variavel2 - variavel3)
+console.log()//Para dar espaço (O \n não funcionou)
+
+console.log(typeof variavel1)
+console.log(typeof variavel2)
+variavel2 = 5
+console.log(typeof variavel3)
+console.log()//Para dar espaço (O \n não funcionou)
+
+console.log(variavel2)
+
+const gata = {
+    Nome:"Vic",
+    Numero: 8987606,
+    Idade: 18
+}
+
+console.log(gata)
+console.log(
+    "Nome:", gata.nome,
+    "Idade", gata.Idade
+)
+
+console.log()//Para dar espaço (O \n não funcionou)
+
+
+let cachorros = ["Bug", "Zeus", "Zaratrusta"]
+console.log(cachorros[1])
